34189824055b8dcc7461076ea5a7e7ee7314ff64
Nicolas Vigier <boklm@torproject.org>
2017-01-20 16:16:26 +0100

    Add projects/tor-browser

---
+++ projects/tor-browser/ddmg.sh
@@ -0,0 +1,18 @@
+[% SET src = c('dmg_src', { error_if_undef => 1 }) -%]
+find [% src %] -executable -exec chmod 0755 {} \;
+find [% src %] ! -executable -exec chmod 0644 {} \;
+
+find [% src %] -exec [% c("var/touch") %] {} \;
+
+dmg_tmpdir=$(mktemp -d)
+[% SET filelist = '"$dmg_tmpdir/filelist.txt"' %]
+pushd [% src %] 
+find . -type f | sed -e 's/^\.\///' | sort | xargs -i echo "{}={}" > [% filelist %]
+find . -type l | sed -e 's/^\.\///' | sort | xargs -i echo "{}={}" >> [% filelist %]
+
+genisoimage -D -V "Tor Browser" -no-pad -R -apple -o "$dmg_tmpdir/tbb-uncompressed.dmg" -path-list [% filelist %] -graft-points -gid 20 -dir-mode 0755 -new-dir-mode 0755
+
+dmg dmg "$dmg_tmpdir/tbb-uncompressed.dmg" [% c('dmg_out', { error_if_undef => 1 }) %]
+popd
+
+rm -Rf "$dmg_tmpdir"
