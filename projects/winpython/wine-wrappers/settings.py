0c23af0c8c287a79a195de3df97bb70488e6ab23
Georg Koppen <gk@torproject.org>
2019-02-03 14:02:47 +0000

    Bug 29319: Remove FTE support for Windows
    We want to use our new mingw-w64/clang toolchain for our Windows
    cross-compilation, which requires a newer Wine among other updated
    components. We need to switch to Debian Stretch for that which is
    necessary anyway sooner or later given that Jessie is not properly
    maintained anymore.
    
    Updating to Stretch breaks FTE and fixing the breakage seems
    non-trivial. We therefore remove support for Windows where it existed
    (there was no 64bit support implemented yet, see: #24195) as it is
    planned to replace FTE with a new pluggable transport in the near future
    anyway.

--- projects/winpython/wine-wrappers/settings.py
+++
@@ -1,2 +0,0 @@
-LD_PRELOAD = "/usr/lib/faketime/libfaketime.so.1"
-FAKETIME = "2000-01-01 00:00:00"
--- projects/winpython/wine-wrappers/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-import py2exe
-setup(
-    console=["gcc.py", "g++.py", "dllwrap.py"],
-    zipfile=None,
-    options={"py2exe": {"bundle_files": 1, "compressed": True}}
-)
