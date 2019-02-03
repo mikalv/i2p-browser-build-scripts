602c18e7308bc9cfa4d41edb291d1f7dbb34255f
Mikal Villa <mikalv@mikalv.net>
2019-02-03 08:56:31 +0100

    Refactoring part 1

---
+++ projects/i2p-browser/ddmg.sh
@@ -0,0 +1,21 @@
+[% SET src = c('dmg_src', { error_if_undef => 1 }) -%]
+find [% src %] -executable -exec chmod 0755 {} \;
+find [% src %] ! -executable -exec chmod 0644 {} \;
+
+find [% src %] -exec [% c("var/touch") %] {} \;
+
+dmg_tmpdir=\$(mktemp -d)
+[% SET filelist = '"\$dmg_tmpdir/filelist.txt"' %]
+pushd [% src %]
+find . -type f | sed -e 's/^\.\///' | sort | xargs -i echo "{}={}" > [% filelist %]
+find . -type l | sed -e 's/^\.\///' | sort | xargs -i echo "{}={}" >> [% filelist %]
+
+export LD_PRELOAD=[% c("var/faketime_path") %]
+export FAKETIME="[% USE date; GET date.format(c('timestamp'), format = '%Y-%m-%d %H:%M:%S') %]"
+
+genisoimage -D -V "I2P Browser" -no-pad -R -apple -o "\$dmg_tmpdir/i2bb-uncompressed.dmg" -path-list [% filelist %] -graft-points -gid 20 -dir-mode 0755 -new-dir-mode 0755
+
+dmg dmg "\$dmg_tmpdir/i2bb-uncompressed.dmg" [% c('dmg_out', { error_if_undef => 1 }) %]
+popd
+
+rm -Rf "\$dmg_tmpdir"
